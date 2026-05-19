2b3622db40413086de9bda2baa64f641bb00ad50
Aaron  Akinwande <aaronakinwande@gmail.com>
2026-05-19 15:57:41 +0100

    Origin    allow and base   url

--- frontend/src/app/lib/api.ts
+++ frontend/src/app/lib/api.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:8000" });
+// const api = axios.create({ baseURL: "http://localhost:8000" });
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+})
 
 export interface Transaction {
   id: string;
